64cc4e3545008d63ad29ff8913cf4884b1b5c889
Dmitriy <qmn277@gmail.com>
2022-04-24 20:28:07 +0300

    Plugin start version
    Successfully replaces mappings.
    TODO: works only on java files, java class must contain @RestController annotation

---
+++ src/main/java/com/github/quarix/mappingreplacement/NotifyReplacement.java
@@ -0,0 +1,15 @@
+package com.github.quarix.mappingreplacement;
+
+import com.intellij.notification.NotificationGroupManager;
+import com.intellij.notification.NotificationType;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.Nullable;
+
+public class NotifyReplacement {
+    public static void notifyInfo(@Nullable Project project, String title, String content, NotificationType type) {
+        NotificationGroupManager.getInstance()
+                .getNotificationGroup("Replacement Notification Group")
+                .createNotification(title, content, type)
+                .notify(project);
+    }
+}
---
+++ src/main/java/com/github/quarix/mappingreplacement/ReplaceAction.java
@@ -0,0 +1,67 @@
+package com.github.quarix.mappingreplacement;
+
+import com.intellij.lang.Language;
+import com.intellij.lang.java.JavaLanguage;
+import com.intellij.notification.NotificationType;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+
+import static com.github.quarix.mappingreplacement.NotifyReplacement.notifyInfo;
+import static com.intellij.openapi.command.WriteCommandAction.runWriteCommandAction;
+
+
+public class ReplaceAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull final AnActionEvent e) {
+        //todo: context menu option only for java files
+        final Editor editor = e.getData(CommonDataKeys.EDITOR);
+        final Language language = e.getData(CommonDataKeys.LANGUAGE);
+
+        final String title = "@RequestMapping Replacement";
+        final Project project = e.getProject();
+
+        if (language == null || !language.is(JavaLanguage.INSTANCE)) {
+            notifyInfo(project, title, "Not java file", NotificationType.INFORMATION);
+            return;
+        }
+
+        if (editor != null) {
+
+            final Document document = editor.getDocument();
+            final String text = document.getText();
+            final String[] methods = {"Get", "Post", "Put", "Delete", "Patch"};
+            String newText = text;
+            for (String method : methods) {
+                newText = getReplacementString(newText, method);
+            }
+            final String result = newText;
+            runWriteCommandAction(project, () ->
+                    document.setText(result)
+            );
+            notifyInfo(project, title, text.equals(result) ? "Nothing has been replaced" : "Replacement complete", NotificationType.INFORMATION);
+        } else {
+            notifyInfo(project, title, "Empty editor!", NotificationType.WARNING);
+        }
+    }
+
+    @NotNull
+    private String getReplacementString(final String text, final String method) {
+        return text.replaceAll("@RequestMapping\\(((.*),)?\\s*method\\s*=\\s*RequestMethod." +
+                        method.toUpperCase(Locale.ROOT) + "\\s*\\)",
+                "@" + method + "Mapping\\($2\\)");
+    }
+
+
+    @Override
+    public boolean isDumbAware() {
+        return false;
+    }
+}
